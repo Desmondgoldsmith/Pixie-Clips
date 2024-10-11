1b74fb06169a7f1ec14ed2eb064d41d1aa5d1b00
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-11 15:55:19 +0000

    chore/ writing out the schema

--- config/schema.ts
+++ config/schema.ts
@@ -1,9 +1,11 @@
 import { boolean, pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
-export const Users = pgTable("user_table", {
+export const usersTable = pgTable("user_table", {
   id: serial("id").primaryKey(),
   name: varchar("name").notNull(),
   email: varchar("email").notNull(),
   imageURL: varchar("imageURL"),
   subscription: boolean("subscription").default(false),
 });
+
+export type InsertUser = typeof usersTable.$inferInsert;
