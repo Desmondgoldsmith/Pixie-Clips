3d28e791367b0ce70c5513464c367940fd18151b
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-11 18:01:18 +0000

    chore/ writing out the schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-config({ path: ".env" });
+config({ path: ".env.local" });
 
 export default defineConfig({
   schema: "./config/schema.ts",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.NEXT_PUBLIC_DRIZZLE_NEON_DATABASE_URL!,
+    url: process.env.DRIZZLE_NEON_DATABASE_URL!,
   },
 });
