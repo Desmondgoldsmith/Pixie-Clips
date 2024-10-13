76fc85bf266fd27409638b8c7b4f2fbe0c43abac
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-13 14:46:07 +0000

    Feat/ working on the signIn UI

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -5,17 +5,15 @@ export default function Page() {
   return (
     <div className="relative min-h-screen flex items-center justify-center">
       <Image
-        src="public/images/pixie-clips-login.png"
+        src="/images/pixie-clips-login.png"
         alt="Background"
-        layout="fill"
-        objectFit="cover"
+        fill
+        style={{ objectFit: "cover" }}
         quality={100}
         priority
       />
-      <div className="absolute inset-0 bg-black opacity-50" />
-      <div className="z-10 bg-white p-8 rounded-lg shadow-xl">
-        <SignIn />
-      </div>
+      <div className="absolute" />
+      <SignIn />
     </div>
   );
 }
