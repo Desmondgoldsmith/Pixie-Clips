cad35877cd75cdb5810ca826e732c2e317aa3363
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-13 17:01:28 +0000

    Feat/ working on the signIn UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Provider } from "./provider";
+import { Space_Grotesk } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Pixie Clips",
   description: "Your favorite text to video generator.",
 };
 
+const grotesk = Space_Grotesk({ subsets: ["latin"] });
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,9 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={grotesk.className}>
           <Provider>{children}</Provider>
         </body>
       </html>
