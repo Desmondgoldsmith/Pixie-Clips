92b003b5ef9b9f7cd20f581be4e9e1425111aced
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-13 15:33:46 +0000

    Feat/ working on the signIn UI

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,3 +1,30 @@
+"use-client";
+import { db } from "@/config/db";
+import { usersTable } from "@/config/schema";
+import { useUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { useEffect } from "react";
+
 export const Provider = ({ children }: any) => {
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && isNewUser;
+  }, [user]);
+
+  const isNewUser = async () => {
+    const userResult = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, user?.primaryEmailAddress?.emailAddress));
+
+    if (!userResult[0]) {
+      await db.insert(usersTable).Values({
+        name: user?.fullName,
+        email: user?.primaryEmailAddress?.emailAddress,
+        imageUrl: user?.imageUrl,
+      });
+    }
+  };
   return <div>{children}</div>;
 };
