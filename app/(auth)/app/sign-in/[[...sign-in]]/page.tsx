c273dc5328c1a420975e622c761bb5dac7bfae8b
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-13 14:16:27 +0000

    Feat/ working on the signIn UI

--- app/(auth)/app/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,21 @@
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="relative min-h-screen flex items-center justify-center">
+      <Image
+        src="public/images/pixie-clips-login.png"
+        alt="Background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        priority
+      />
+      <div className="absolute inset-0 bg-black opacity-50" />
+      <div className="z-10 bg-white p-8 rounded-lg shadow-xl">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
