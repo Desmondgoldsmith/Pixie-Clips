d1a126d4a0a01c2351c8e3d36b7f585375a59399
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-14 02:36:13 +0000

    Feat/ working on the dashboard UI

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const DashboardLayout = () => {
-  return <div>DashboardLayout</div>;
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
 };
 
 export default DashboardLayout;
