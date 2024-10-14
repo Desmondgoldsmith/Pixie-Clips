b8b1985e7f696141ead0db3f0edce62f92b5bd93
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-14 03:55:12 +0000

    Feat/ working on the dashboard UI

--- app/dashboard/_component/header.tsx
+++ app/dashboard/_component/header.tsx
@@ -1,32 +1,33 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import { UserButton } from "@clerk/nextjs";
 const Header = () => {
   return (
-    <header className="bg-primary text-white shadow-md">
+    <header className="bg-primary-dark text-white shadow-md">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href="/dashboard" className="flex-shrink-0">
               <Image
                 src="/images/pixie-clips.png"
-                alt="Logo"
-                width={100}
-                height={100}
+                alt="pixie clips"
+                width={80}
+                height={80}
                 className="rounded-full"
               />
             </Link>
           </div>
           <div className="flex items-center">
-            <span className="mr-4 text-sm font-medium">John Doe</span>
+            {/* <span className="mr-4 text-sm font-medium">John Doe</span>
             <Image
               src="/path-to-user-image.jpg"
               alt="User"
               width={32}
               height={32}
               className="rounded-full border-2 border-secondary"
-            />
+            /> */}
+            <UserButton />
           </div>
         </div>
       </div>
