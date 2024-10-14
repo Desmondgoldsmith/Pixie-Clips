2b02061c07543c597900f8c6ea0b526f6033b981
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-14 04:09:58 +0000

    Feat/ working on the dashboard UI

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./_component/header";
+import Sidebar from "./_component/sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      {children}
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 p-4 md:p-6">{children}</main>
+      </div>
     </div>
   );
 };
