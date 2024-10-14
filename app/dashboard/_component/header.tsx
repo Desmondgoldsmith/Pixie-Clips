81e5e87cf4e00bbea99922b09a5b7af410766974
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-14 08:39:34 +0000

    Feat/ working on the dashboard UI

--- app/dashboard/_component/header.tsx
+++ app/dashboard/_component/header.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 const Header = () => {
   return (
-    <header className="bg-primary-dark text-white shadow-md">
-      <div className="container mx-auto px-4">
+    <header className="bg-primary text-white shadow-md">
+      <div className="container mx-auto px-2">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href="/dashboard" className="flex-shrink-0">
@@ -18,15 +19,10 @@ const Header = () => {
               />
             </Link>
           </div>
-          <div className="flex items-center">
-            {/* <span className="mr-4 text-sm font-medium">John Doe</span>
-            <Image
-              src="/path-to-user-image.jpg"
-              alt="User"
-              width={32}
-              height={32}
-              className="rounded-full border-2 border-secondary"
-            /> */}
+          <div className="flex items-center space-x-3">
+            <Button className="bg-secondary-light text-primary-dark">
+              Dashboard
+            </Button>
             <UserButton />
           </div>
         </div>
--- app/dashboard/_component/sidebar.tsx
+++ app/dashboard/_component/sidebar.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import React from "react";
 import Link from "next/link";
 import { LayoutDashboard, PlusCircle, User } from "lucide-react";
+import {usePathname} from 'next/navigation'
 
 const sidebarItems = [
   { icon: LayoutDashboard, label: "Dashboard", href: "/dashboard" },
@@ -8,16 +11,19 @@ const sidebarItems = [
   { icon: User, label: "Account", href: "/account" },
 ];
 
+
 const Sidebar = () => {
+  const path = usePathname()
+
   return (
-    <aside className="bg-primary text-white w-64 min-h-screen hidden md:block">
+    <aside className="bg-primary text-white w-64 min-h-screen hidden md:block shadow-md">
       <nav className="p-4">
         <ul className="space-y-2">
           {sidebarItems.map((item) => (
             <li key={item.label}>
               <Link
                 href={item.href}
-                className="flex items-center space-x-3 p-2 rounded-lg hover:bg-primary-light transition-colors"
+                className= {`flex items-center space-x-3 p-2 rounded-lg hover:bg-primary-light transition-colors ${path === item.href ? `bg-primary-light`: ''}`}
               >
                 <item.icon className="w-5 h-5" />
                 <span>{item.label}</span>
