05d59e8f85135f6c9aa23d1b8881abdfd9daea48
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-17 21:26:39 +0000

    chore/ writing out the generate audio api

--- app/api/get-audio/route.tsx
+++ app/api/get-audio/route.tsx
@@ -1,5 +1,17 @@
 import textToSpeech from "@google-cloud/text-to-speech";
 
-const client = new textToSpeech.textToSpeechClient({});
+const client = new textToSpeech.textToSpeechClient({
+  apiKey: process.env.GOOGLE_API_KEY,
+});
 
-export const POST = async (request: Request) => {};
+export const POST = async (request: Request) => {
+  const { text, id } = await request.json();
+  // Construct the request
+  const getRequest = {
+    input: { text: text },
+    // Select the language and SSML voice gender (optional)
+    voice: { languageCode: "en-US", ssmlGender: "NEUTRAL" },
+    // select the type of audio encoding
+    audioConfig: { audioEncoding: "MP3" },
+  };
+};
